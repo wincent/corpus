e44f455c9d2efbc66d4c244b36c73a815d6a14d1
Greg Hurrell <greg@hurrell.net>
2015-05-05 21:32:18 -0700

    Don't crash gulp during watches
    I am not sure how this works seeing as it doesn't seem to be documented,
    but it does work...
    
    According to this, everything will suck until gulp 4:
    
      https://github.com/gulpjs/gulp/issues/71
    
    This is the page where I found the reference to `fatalLevel`:
    
      http://www.artandlogic.com/blog/2014/05/error-handling-in-gulp/

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var babel = require('gulp-babel');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var minifyHTML = require('gulp-minify-html');
 
 gulp.task('default', ['watch']);
 
 gulp.task('watch', function() {
+  fatalLevel = 'off'; // provided by gutil; don't crash during watch
   gulp.watch('src/**/*.html', ['html']);
   gulp.watch('src/**/*.js', ['js']);
 });
