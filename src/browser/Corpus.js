7a16b2dc9165dcc40366609083edaf0e0bf65c3c
Greg Hurrell <greg@hurrell.net>
2015-05-05 20:01:29 -0700

    Hacky attempt at Babel-ifying in the browser
    Committing this for posterity, you can see a couple of different hacky
    things I did trying to get this to work.
    
    I'll back this out once I figure out the right way to do it. I suspect
    that the most appropriate choice will be a build process that dumps
    something in a "dist" directory. At some point I'll want to turn on
    "production" mode as well so that the `__DEV__` checks in React get
    skipped over (or ideally even stripped out).

--- src/browser/Corpus.js
+++ src/browser/Corpus.js
@@ -7,6 +7,7 @@ var mainWindow = null; // global reference to avoid premature GC
 var Corpus = {
   start() {
     app.on('ready', () => {
+      global.node_modules = 'file://' + __dirname + '/../../node_modules';
       mainWindow = new BrowserWindow({height: 800, show: false, width: 1200});
       mainWindow.loadUrl('file://' + __dirname + '/../index.html');
       mainWindow.show();
