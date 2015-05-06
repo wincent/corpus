0986b9c679bb40208aeacda93ac99331ce4b841e
Greg Hurrell <greg@hurrell.net>
2015-05-05 23:28:01 -0700

    Add rudimentary menu
    Based on the template sample at:
    
      https://github.com/atom/electron/blob/master/docs/api/menu.md

--- src/browser/main.js
+++ src/browser/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
 import BrowserWindow from 'browser-window';
+import Menu from 'menu';
 import app from 'app';
+
+import menu from './menu';
 
 let mainWindow = null; // global reference to avoid premature GC
 
@@ -9,6 +12,10 @@ app.on('ready', () => {
   mainWindow = new BrowserWindow({height: 800, show: false, width: 1200});
   mainWindow.loadUrl('file://' + __dirname + '/../index.html');
   mainWindow.show();
+
+  Menu.setApplicationMenu(
+    Menu.buildFromTemplate(menu)
+  );
 
   mainWindow.on('closed', () => {
     mainWindow = null; // allow GC
---
+++ src/browser/menu.js
@@ -0,0 +1,41 @@
+import app from 'app';
+
+const menu = [
+  {
+    label: 'Corpus',
+    submenu: [
+      {
+        label: 'About Corpus',
+        selector: 'orderFrontStandardAboutPanel:',
+      },
+      {type: 'separator'},
+         {
+        label: 'Services',
+        submenu: [],
+      },
+      {type: 'separator'},
+      {
+        accelerator: 'Command+H',
+        label: 'Hide Corpus',
+        selector: 'hide:',
+      },
+      {
+        accelerator: 'Alt+Command+H',
+        label: 'Hide Others',
+        selector: 'hideOtherApplications:',
+      },
+      {
+        label: 'Show All',
+        selector: 'unhideAllApplications:',
+      },
+      {type: 'separator'},
+      {
+        accelerator: 'Command+Q',
+        click: () => app.quit(),
+        label: 'Quit Corpus',
+      },
+    ],
+  },
+];
+
+export default menu;
