06c704e6dbb0a55f81de0cfa6b6b9e8bdd4d5f46
Greg Hurrell <greg@hurrell.net>
2015-05-06 17:11:32 -0700

    Don't show window until it's fit for showing

--- src/browser/main.js
+++ src/browser/main.js
@@ -11,7 +11,7 @@ let mainWindow = null; // global reference to avoid premature GC
 app.on('ready', () => {
   mainWindow = new BrowserWindow({height: 800, show: false, width: 1200});
   mainWindow.loadUrl('file://' + __dirname + '/../index.html');
-  mainWindow.show();
+  mainWindow.webContents.on('did-finish-load', () => mainWindow.show());
 
   Menu.setApplicationMenu(
     Menu.buildFromTemplate(menu)
