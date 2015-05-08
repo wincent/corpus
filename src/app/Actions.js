77cecba8a101785eef30503e6129276c77ff1d95
Greg Hurrell <greg@hurrell.net>
2015-05-07 21:08:29 -0700

    Actually route actions through the Dispatcher
    Still probably using Flux wrong, but this is a step in the right
    direction.

---
+++ src/app/Actions.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import keyMirror from 'react/lib/keyMirror';
+
+const Actions = keyMirror({
+  NEXT_NOTE_SELECTED: null,
+  NOTE_SELECTED: null,
+  PREVIOUS_NOTE_SELECTED: null,
+});
+
+export default Actions;
--- src/app/stores/NotesSelectionStore.js
+++ src/app/stores/NotesSelectionStore.js
@@ -3,26 +3,49 @@
 import {EventEmitter} from 'events';
 import ipc from 'ipc';
 
+import Actions from '../Actions';
+import Dispatcher from '../Dispatcher';
 import NotesStore from './NotesStore';
 
 // TODO: persist this across restarts
 // TODO: support null selection
 let currentSelectionIndex = 0;
 
-ipc.on('next', () => {
+function incrementCurrentSelectionIndex() {
   const maxSelectionIndex = NotesStore.getNotes().length - 1;
   if (currentSelectionIndex < maxSelectionIndex) {
     currentSelectionIndex++;
     NoteSelectionStore.emit('change');
   }
-});
+}
 
-ipc.on('previous', () => {
+function decrementCurrentSelectionIndex() {
   if (currentSelectionIndex) {
     currentSelectionIndex--;
     NoteSelectionStore.emit('change');
   }
+}
+
+Dispatcher.register(payload => {
+  switch (payload.type) {
+    case Actions.NEXT_NOTE_SELECTED:
+      incrementCurrentSelectionIndex();
+      break;
+    case Actions.NOTE_SELECTED:
+      break;
+    case Actions.PREVIOUS_NOTE_SELECTED:
+      decrementCurrentSelectionIndex();
+      break;
+  }
 });
+
+ipc.on('next', () => (
+  Dispatcher.dispatch({type: Actions.NEXT_NOTE_SELECTED})
+));
+
+ipc.on('previous', () => (
+  Dispatcher.dispatch({type: Actions.PREVIOUS_NOTE_SELECTED})
+));
 
 const NoteSelectionStore = Object.assign({
   getCurrentSelectionIndex() {
