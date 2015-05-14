7f5076b4818b817b3a2efe995521ce8c5e4e022f
Greg Hurrell <greg@hurrell.net>
2015-05-14 07:27:14 -0700

    Add Mouse module
    Which I forgot to include in a previous commit.

---
+++ src/app/Mouse.js
@@ -0,0 +1,9 @@
+// Copyright 2015-present Greg Hurrell. All rights reserved.
+// Licensed under the terms of the MIT license.
+
+'use strict';
+
+export default {
+  LEFT_BUTTON: 0,
+  RIGHT_BUTTON: 2,
+};
