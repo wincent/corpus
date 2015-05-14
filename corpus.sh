08423a298aff8e905d8acf772c37a43861bad20f
Greg Hurrell <greg@hurrell.net>
2015-05-14 07:11:36 -0700

    Use NODE_ENV=production by default
    To make React fast. If you want to debug something specifically, start
    with:
    
      NODE_ENV= ./corpus.sh
      NODE_ENV=anything_but_production ./corpus.sh
    
    Still need to figure out how to apply this when bundling the code for
    the release of the app.

--- corpus.sh
+++ corpus.sh
@@ -5,4 +5,14 @@
 ELECTRON_DIST=/usr/local/lib/node_modules/electron-prebuilt/dist
 ELECTRON_EXECUTABLE=Electron.app/Contents/MacOS/Electron
 
-$ELECTRON_DIST/$ELECTRON_EXECUTABLE .
+# Check if NODE_ENV is set (http://stackoverflow.com/a/13864829/2103996).
+if [ -z ${NODE_ENV+unset} ]; then
+  # Fast by default, to override, start with:
+  #
+  #   NODE_ENV= ./corpus.sh
+  #   NODE_ENV=anything_but_production ./corpus.sh
+  #
+  NODE_ENV=production $ELECTRON_DIST/$ELECTRON_EXECUTABLE .
+else
+  NODE_ENV="${NODE_ENV}" $ELECTRON_DIST/$ELECTRON_EXECUTABLE .
+fi
