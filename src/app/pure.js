ef63a76aaf49301e192fcd5b2579424d53314c12
Greg Hurrell <greg@hurrell.net>
2015-05-13 22:01:41 -0700

    Make @pure decorator collaborate with component method
    I was thinking I might need this for some `forceUpdate` shenannigans
    (specifically, trying to get the `NotePreview` to focus when
    right-clicked, before the contextual menu shows up), but that was
    starting to get real messy, and I'm skeptical that it would actually
    work in the long run (as the menu lag issues I am fighting against seem
    pretty deeply grained).
    
    Nevertheless, I see this as being pretty useful in a Flux-driven
    application for the reasons noted in the docs. Let's ship it!

--- src/app/pure.js
+++ src/app/pure.js
@@ -5,19 +5,38 @@
 
 import shallowEqual from 'react/lib/shallowEqual';
 
-function shouldComponentUpdate(nextProps, nextState) {
-  return (
-    !shallowEqual(this.props, nextProps) ||
-    !shallowEqual(this.state, nextState)
-  );
-}
-
+/**
+ * Implements a basic `shouldComponentUpdate` based on shallow comparison of
+ * props and state. If the component is a "pure" function of its props and
+ * state, it need not be updated unless those change.
+ *
+ * If a component implements its own `shouldComponentUpdate`, it may be called
+ * as well. This is useful in a Flux world, where a component may wish to query
+ * a store independently of props and state in order to decide whether to
+ * render:
+ *
+ * - if shallow equality check of props and state fails, we return `true`; the
+ *   component's `shouldComponentUpdate` method is not called
+ * - if shallow equality check of props and state succeeds, and the component
+ *   defined its own `shouldComponentUpdate, we call that and return the answer
+ */
 export default function pure(target) {
-  if (target.prototype.hasOwnProperty('shouldComponentUpdate')) {
-    throw new Error(
-      target.name +
-      " already implements shouldComponentUpdate: can't use @pure"
+  let original = target.prototype.shouldComponentUpdate;
+
+  function shouldComponentUpdate(nextProps, nextState) {
+    const pureResult = (
+      !shallowEqual(this.props, nextProps) ||
+      !shallowEqual(this.state, nextState)
     );
-  }
+
+    if (pureResult) {
+      return true;
+    } else if (original) {
+      return original(nextProps, nextState);
+    } else {
+      return false;
+    }
+  };
+
   target.prototype.shouldComponentUpdate = shouldComponentUpdate;
-};
+}
