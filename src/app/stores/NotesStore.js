e159b0cef48991a197abfa025a57b971ee13df8b
Greg Hurrell <greg@hurrell.net>
2015-05-15 21:45:04 -0700

    Load notes in batches to improve responsiveness
    Load a viewport's worth first, then the rest.
    
    Doesn't make a huge difference, as loading it fast, but it might make it
    later.

--- src/app/stores/NotesStore.js
+++ src/app/stores/NotesStore.js
@@ -10,12 +10,21 @@ import path from 'path';
 import process from 'process';
 
 import Actions from '../Actions';
+import NotePreview from '../NotePreview.react'; // smell: getting constant from view
 import Store from './Store';
 import handleError from '../handleError';
 
 const readdir = Promise.promisify(fs.readdir);
 const readFile = Promise.promisify(fs.readFile);
 const stat = Promise.promisify(fs.stat);
+
+/**
+ * The number of notes to load immediately on start-up. Remaining notes are
+ * retrieved in a second pass.
+ *
+ * Intended to increase perceived responsiveness.
+ */
+const PRELOAD_COUNT = Math.floor(window.innerHeight / NotePreview.ROW_HEIGHT) + 5;
 
 /**
  * Ordered colllection of notes (as they appear in the NoteList).
@@ -27,40 +36,77 @@ let notes = Immutable.List();
  */
 let noteID = 0;
 
+const notesDir = path.join(process.env.HOME, 'Documents', 'Notes');
+
+function ignore(): void {
+  return;
+};
+
+function filterFilenames(filenames: Array<string>): Array<string> {
+  return filenames.filter(fileName => path.extname(fileName) === '.txt');
+}
+
+function getStatInfo(fileName: string): Immutable.Map {
+  const notePath = path.join(notesDir, fileName);
+  const title = path.basename(notePath, '.txt');
+  return Promise.join(
+    noteID++,
+    title,
+    notePath,
+    stat(notePath).catch(ignore),
+    (id, title, path, stat) => Immutable.Map({
+      id,
+      mtime: stat && stat.mtime.getTime(),
+      path,
+      title,
+    })
+  );
+}
+
+function compareMTime(a, b) {
+  const aTime = a.get('mtime');
+  const bTime = b.get('mtime');
+  if (aTime > bTime) {
+    return -1;
+  } else if (aTime < bTime) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+function readContents(info) {
+  return Promise.join(
+    info,
+    readFile(info.get('path')).catch(ignore),
+    (info, text) => info.set('text', text.toString())
+  );
+}
+
+function appendResults(results) {
+  if (results.length) {
+    notes = notes.push(...results);
+    Actions.notesLoaded();
+  }
+}
+
 class NotesStore extends Store {
   constructor() {
     super();
 
-    const ignore = () => {};
-    const notesDir = path.join(process.env.HOME, 'Documents', 'Notes');
-
     readdir(notesDir)
-      .filter(
-        fileName => path.extname(fileName) === '.txt',
-        {concurrency: Infinity}
-      )
-      .map(
-        fileName => {
-          const notePath = path.join(notesDir, fileName);
-          const title = path.basename(notePath, '.txt');
-          return Promise.join(
-            noteID++,
-            title,
-            readFile(notePath).catch(ignore),
-            stat(notePath).catch(ignore),
-            (id, title, text, stat) => Immutable.Map({
-              id,
-              mtime: stat && stat.mtime.getTime(),
-              text: text && text.toString(),
-              title,
-            })
-          );
-        },
-        {concurrency: 5}
-      )
-      .then(data => {
-        notes = notes.push(...data).sortBy(note => note.get('mtime')).reverse();
-        Actions.notesLoaded();
+      .then(filterFilenames)
+      .map(getStatInfo)
+      .then(info => {
+        const sorted = info.sort(compareMTime);
+        const preload = sorted.slice(0, PRELOAD_COUNT);
+        return new Promise((resolve, reject) => (
+          Promise.map(preload, readContents, {concurrency: 5})
+            .then(appendResults)
+            .then(() => Promise.map(sorted, readContents, {concurrency: 5}))
+            .then(appendResults)
+            .then(resolve)
+        ));
       })
       .catch(error => handleError(error, 'Failed to read notes from disk'));
   }
