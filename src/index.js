5cae7339585867929faf282569cb8651b56a6a15
Greg Hurrell <greg@hurrell.net>
2015-05-18 21:56:09 -0700

    Prefer const over let

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@
 import React from 'react';
 import Corpus from './app/Corpus.react';
 
-let root = document.getElementById('app-root');
+const root = document.getElementById('app-root');
 React.render(<Corpus />, root);
