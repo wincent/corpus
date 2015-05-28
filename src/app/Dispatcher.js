1322e859aab9370dc7857a62aedc4d44d10734e1
Greg Hurrell <greg@hurrell.net>
2015-05-27 22:58:17 -0700

    Kill some parens
    I don't like parens.

--- src/app/Dispatcher.js
+++ src/app/Dispatcher.js
@@ -10,8 +10,8 @@
 import {Dispatcher} from 'flux';
 import {batchedUpdates} from 'react/lib/ReactUpdates';
 
-export default new (class extends Dispatcher {
+export default new class extends Dispatcher {
   dispatch(payload) {
     batchedUpdates(() => super.dispatch(payload));
   }
-})();
+}();
