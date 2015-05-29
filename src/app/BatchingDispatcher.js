e31ec1f52bcecd30a61d91469c771469ca614087
Greg Hurrell <greg@hurrell.net>
2015-05-28 18:13:29 -0700

    Extract BatchingDispatcher into separate file
    This resolves my concern with the class name not matching the file name.

---
+++ src/app/BatchingDispatcher.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2015-present Greg Hurrell. All rights reserved.
+ * Licensed under the terms of the MIT license.
+ *
+ * @flow
+ */
+
+'use strict';
+
+import {Dispatcher} from 'flux';
+import {batchedUpdates} from 'react/lib/ReactUpdates';
+
+export default class BatchingDispatcher extends Dispatcher {
+  dispatch(payload) {
+    batchedUpdates(() => super.dispatch(payload));
+  }
+}
--- src/app/Dispatcher.js
+++ src/app/Dispatcher.js
@@ -7,13 +7,6 @@
 
 'use strict';
 
-import {Dispatcher} from 'flux';
-import {batchedUpdates} from 'react/lib/ReactUpdates';
-
-class BatchingDispatcher extends Dispatcher {
-  dispatch(payload) {
-    batchedUpdates(() => super.dispatch(payload));
-  }
-}
+import BatchingDispatcher from './BatchingDispatcher';
 
 export default new BatchingDispatcher();
