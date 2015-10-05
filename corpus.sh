4c12677573ac97a0cc8f0b9078a972d41ef4756c
Greg Hurrell <greg@hurrell.net>
2015-10-04 23:37:28 -0700

    Fix ./corpus.sh: line 20: [: =: unary operator expected

--- corpus.sh
+++ corpus.sh
@@ -17,7 +17,7 @@ if [ -z ${NODE_ENV+unset} ]; then
   #   NODE_ENV= ./corpus.sh
   #   NODE_ENV=anything_but_production ./corpus.sh
   #
-  if [ $1 = '--daemonize' ]; then
+  if [[ $1 = '--daemonize' ]]; then
     (NODE_ENV=production $ELECTRON_DIST/$ELECTRON_EXECUTABLE . &) &
   else
     NODE_ENV=production $ELECTRON_DIST/$ELECTRON_EXECUTABLE .
