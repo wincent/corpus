wincent/corpus

commit a2c5d406db1fa5c7839a77d011db879026ec0778

Fix regression with note focusing after note creation

src/app/stores/FocusStore.js
@@ -21,7 +21,12 @@ class FocusStore extends Store {
       case Actions.NOTE_CREATION_COMPLETED:
         this.waitFor(NotesSelectionStore.dispatchToken);
         focus = 'Note';
-        this.emit('change');
+
+        // Due to batched updates, we have to delay actually firing the change
+        // event here, otherwise it will fire before the Note view has even
+        // rendered and started listening to this store, which means that focus
+        // won't actually transfer.
+        setImmediate(() => this.emit('change'));
         break;
 
       case Actions.NOTE_FOCUS_REQUESTED:

commit 0fd47993ef0fe14608cfcc7b0561cc10195065f7

Apply alternative fix for note creation focus regression
This is an alternative and probably less sketchy fix for the regression
originally corrected in a2c5d406db1fa. I'm trying to port these Flux
stores over to Redux reducers one by one, and eliminating asynchrony (in
this case, the `setImmediate` call) is an important step.

(Alas, I think `waitFor` is a spanner in the works, because it means you
can't convert any reducer that uses it, instead having to traverse up
the dependency parent chain until you hit the root...)

src/app/components/ContentEditable.react.js
@@ -38,6 +38,7 @@ class ContentEditable extends React.Component {
 
   componentDidMount() {
     FocusStore.on('change', this._updateFocus);
+    this._updateFocus();
   }
 
   componentWillReceiveProps(nextProps) {

src/app/stores/FocusStore.js
@@ -19,12 +19,7 @@ class FocusStore extends Store {
       case Actions.NOTE_CREATION_COMPLETED:
         this.waitFor(NotesSelectionStore.dispatchToken);
         focus = 'Note';
-
-        // Due to batched updates, we have to delay actually firing the change
-        // event here, otherwise it will fire before the Note view has even
-        // rendered and started listening to this store, which means that focus
-        // won't actually transfer.
-        setImmediate(() => this.emit('change'));
+        this.emit('change');
         break;
 
       case Actions.NOTE_FOCUS_REQUESTED: