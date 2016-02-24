654c810700af6630ff794631745e465019087ba9
Greg Hurrell <greg@hurrell.net>
2016-02-23 23:00:48 -0800

    Deal with redux-saga deprecation warning
    `getState` is deprecated. `select` is the blessed way to do this (note
    that I might want to make a "selectors" folder if I start to add more of
    these things.

--- src/app/saga.js
+++ src/app/saga.js
@@ -5,16 +5,17 @@
  * @flow
  */
 
-import {call, put, take} from 'redux-saga/effects';
+import {call, put, select} from 'redux-saga/effects';
 import loadConfig from './loadConfig';
 import querySystem from './querySystem';
 
 /**
  * Root saga, started when application boots.
  */
-export default function* saga(getState) {
-  const config = yield call(loadConfig);
-  yield put({type: 'CONFIG_LOADED', config});
-  const info = yield call(querySystem, getState().config);
+export default function* saga() {
+  const rawConfig = yield call(loadConfig);
+  yield put({type: 'CONFIG_LOADED', config: rawConfig});
+  const config = yield select(state => state.config);
+  const info = yield call(querySystem, config);
   yield put({type: 'SYSTEM_INFO_LOADED', info});
 }
