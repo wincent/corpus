7048fdf906466085d31b1110ce943302a4e94ff2
Greg Hurrell <greg@hurrell.net>
2016-03-12 21:33:18 +0000

    Remove unnecessary lint suppression

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  * @flow
  */
 
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import Corpus from './app/components/Corpus.react';
