42e52a550d9f652e4a8eb81798f96b10b319a7f7
Greg Hurrell <greg@hurrell.net>
2017-02-02 07:52:06 -0800

    Note the need to copy vendor files to the release bundle
    Without this, our font-based icons are all missing.

--- gulpfile.js
+++ gulpfile.js
@@ -146,7 +146,7 @@ gulp.task('copy-debug-plist', ['rename-debug-app'], function() {
   5. Bundle node_modules (eg. NODE_ENV=production + uglify for React)
 
     mkdir -p release/Corpus.app/Contents/Resources/app
-    cp -pR node_modules package.json dist release/Corpus.app/Contents/Resources/app/
+    cp -pR node_modules package.json dist vendor release/Corpus.app/Contents/Resources/app/
 
 */
 
