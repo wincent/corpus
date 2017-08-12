64d300b3c4c3299087ae662756ed12d2229f345a
Greg Hurrell <greg@hurrell.net>
2017-08-11 17:48:35 -0700

    Commit systemSelector
    Not actually using it in any meaningful way, but want to get it out of
    my darn worktree, where it has been annoying me for about a year now.

---
+++ src/app/selectors/systemSelector.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright 2016-present Greg Hurrell. All rights reserved.
+ * Licensed under the terms of the MIT license.
+ *
+ * @flow
+ */
+
+export default function systemSelector(state) {
+  return state.system;
+}
