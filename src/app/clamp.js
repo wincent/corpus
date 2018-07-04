2ed4bd2467933bf1968418c378ad1da1cdd68ccb
Greg Hurrell <greg@hurrell.net>
2018-07-04 00:46:48 -0700

    Make clamp module `@flow strict`

--- src/app/clamp.js
+++ src/app/clamp.js
@@ -2,15 +2,17 @@
  * Copyright 2015-present Greg Hurrell. All rights reserved.
  * Licensed under the terms of the MIT license.
  *
- * @flow
+ * @flow strict
  */
 
-function clamp(value: number, minimum: number, maximum: number): number {
+export default function clamp(
+  value: number,
+  minimum: number,
+  maximum: number,
+): number {
   if (minimum <= maximum) {
     return Math.min(Math.max(value, minimum), maximum);
   } else {
     return NaN;
   }
 }
-
-export default clamp;
