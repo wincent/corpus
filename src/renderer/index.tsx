1a588bc6accae23c297ad21bea3057281376dd67
Greg Hurrell <greg@hurrell.net>
2019-05-02 00:20:07 +0200

    feat: add "watch" mode and top-level component to test it
    To make things not utterly trivial, add a top-level component so that we
    can test out the "watch" mode.

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 
+import Corpus from './components/Corpus';
+
 const root = document.getElementById('root');
 
 if (!root) {
   throw new Error('No #root element');
 }
 
-render(<div>text</div>, root);
+render(<Corpus />, root);
