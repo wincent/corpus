e5fb224a5e96ca89535b200ecfc878ed0d8d054b
Greg Hurrell <greg@hurrell.net>
2019-05-01 21:48:43 +0200

    feat: add ability to run electron app

---
+++ main.js
@@ -0,0 +1,8 @@
+const {app, BrowserWindow} = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({width: 800, height: 600});
+  win.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
