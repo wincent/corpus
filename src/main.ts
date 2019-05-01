8dfad2722eb0a3482545c679d44ee176de4e198c
Greg Hurrell <greg@hurrell.net>
2019-05-01 23:33:20 +0200

    refactor: switch from `require` to `import` style
    We still need to build CommonJS-style `require` statements in order for
    Electron to grok them, but without the `import` in the source, the
    TypeScript compiler doesn't properly type-check.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron');
+import {app, BrowserWindow} from 'electron';
 
 function createWindow() {
   const win = new BrowserWindow({width: 800, height: 600});
