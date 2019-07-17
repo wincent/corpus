1ee223002ee05801887d80294df0b53e5caace7d
Greg Hurrell <greg@hurrell.net>
2019-07-17 23:12:27 +0200

    feat: add Viewport component

--- src/renderer/components/Corpus.tsx
+++ src/renderer/components/Corpus.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import Viewport from './Viewport';
+
 export default function Corpus() {
-  return <div>Corpus</div>;
+  return <Viewport>Corpus</Viewport>;
 }
---
+++ src/renderer/components/Viewport.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    width: '100%',
+  } as React.CSSProperties,
+};
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export default function Viewport({children}: Props) {
+  return <div style={styles.root}>{children}</div>;
+}
