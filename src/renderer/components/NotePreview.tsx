b516d3339e081a30130a2afb241ad4e69e330d22
Greg Hurrell <greg@hurrell.net>
2019-08-07 22:43:41 +0200

    feat: finish off NotePreview styling
    Make `isPrivate` actually work.

--- src/renderer/components/NotePreview.tsx
+++ src/renderer/components/NotePreview.tsx
@@ -5,64 +5,56 @@
 
 import * as React from 'react';
 import {PREVIEW_ROW_HEIGHT} from '../constants';
+import useStyles from '../hooks/useStyles';
 import PreviewTitle from './PreviewTitle';
 import Tags from './Tags';
 
-// TODO: consistently use interface or type for props
 type Props = {
   focused: boolean;
   selected: boolean;
   note: Note;
 };
 
-// TODO: switch to useStyles hook
-function getStyles({
-  focused,
-  selected,
-}: Pick<Props, 'focused' | 'selected'>): Styles<'root' | 'text'> {
-  // const {focused, note, selected} = this.props;
-  // const isPrivate = note.tags.has('private');
-  const isPrivate = false;
-
-  return {
-    root: {
-      background: focused ? '#6f6f73' : selected ? '#c8c8c8' : 'inherit',
-      borderBottom: '1px solid #c0c0c0',
-      fontFamily: 'Helvetica Neue',
-      fontSize: '11px',
-      lineHeight: '14px',
-      listStyleType: 'none',
-      height: PREVIEW_ROW_HEIGHT + 'px',
-      padding: '4px 4px 4px 8px',
-      position: 'relative',
-    },
-    text: {
-      WebkitBoxOrient: 'vertical',
-      WebkitLineClamp: 2,
-      color: isPrivate
-        ? 'transparent'
-        : focused
-        ? '#fff'
-        : selected
-        ? '#4e4e4e'
-        : '#a3a3a3',
-      display: '-webkit-box',
-      fontWeight: 'normal',
-      overflow: 'hidden',
-      textShadow:
-        isPrivate && focused
-          ? '0 0 5px rgba(255, 255, 255, .5)'
-          : isPrivate
-          ? '0 0 5px rgba(0, 0, 0, .25)'
-          : 'unset',
-    },
-  };
-}
-
 export default function NotePreview({focused, selected, note}: Props) {
-  const styles = getStyles({focused, selected});
-
   const {tags, text, title} = note;
+
+  const styles = useStyles<'root' | 'text'>(() => {
+    const isPrivate = tags.has('private');
+
+    return {
+      root: {
+        background: focused ? '#6f6f73' : selected ? '#c8c8c8' : 'inherit',
+        borderBottom: '1px solid #c0c0c0',
+        fontFamily: 'Helvetica Neue',
+        fontSize: '11px',
+        lineHeight: '14px',
+        listStyleType: 'none',
+        height: PREVIEW_ROW_HEIGHT + 'px',
+        padding: '4px 4px 4px 8px',
+        position: 'relative',
+      },
+      text: {
+        WebkitBoxOrient: 'vertical',
+        WebkitLineClamp: 2,
+        color: isPrivate
+          ? 'transparent'
+          : focused
+          ? '#fff'
+          : selected
+          ? '#4e4e4e'
+          : '#a3a3a3',
+        display: '-webkit-box',
+        fontWeight: 'normal',
+        overflow: 'hidden',
+        textShadow:
+          isPrivate && focused
+            ? '0 0 5px rgba(255, 255, 255, .5)'
+            : isPrivate
+            ? '0 0 5px rgba(0, 0, 0, .25)'
+            : 'unset',
+      },
+    };
+  });
 
   return (
     <li style={styles.root}>
