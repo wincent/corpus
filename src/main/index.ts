da12f748c475d39c78ce1f177a2388fd2c1617c5
Greg Hurrell <greg@hurrell.net>
2019-08-08 23:18:34 +0200

    feat: hack in ability to use React dev tools

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,8 @@
  */
 
 import {app, BrowserWindow} from 'electron';
+import * as path from 'path';
+import * as os from 'os';
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -16,7 +18,23 @@ function createWindow() {
     },
     width: 1200,
   });
+
   win.loadFile('index.html');
+
+  // TODO in dev only, add existence checks
+  BrowserWindow.addDevToolsExtension(
+    path.join(
+      os.homedir(),
+      'Library',
+      'Application Support',
+      'Google',
+      'Chrome',
+      'Profile 1',
+      'Extensions',
+      'fmkadmapgofadopljbjfkapdkoienihi',
+      '3.6.0_0',
+    ),
+  );
 }
 
 app.on('ready', createWindow);
