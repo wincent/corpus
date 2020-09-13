wincent/corpus

commit c255bc375ea95689c45c5812e4fda94f33d99b86

fix: make tab completion cycling work outside of Corpus directories
This was getting stuck on second item. Basically, if you call `expand()`
it gets stuck, otherwise it doesn't... (:facepalm:).

So, only call `expand()` if we actually need to (only one place we do
that, which is the ftdetect plug-in).

lua/corpus/init.lua
@@ -216,8 +216,11 @@ corpus = {
         end
       end
     else
-      -- BUG: getting stuck on one of these
-      return corpus.directories()
+      local directories = corpus.directories()
+      return util.list.filter(directories, function(directory, i)
+        -- TODO actually filter here
+        return true
+      end)
     end
   end,
 
@@ -589,7 +592,11 @@ corpus = {
   -- Turns `afile` into a simplified absolute path with all symlinks resolved.
   -- If `afile` corresponds to a directory any trailing slash will be removed.
   normalize = function(afile)
-    local file = vim.fn.fnamemodify(vim.fn.resolve(vim.fn.expand(afile)), ':p')
+    local file = afile
+    if vim.startswith(file, '<') then
+      file = vim.fn.expand(file)
+    end
+    file = vim.fn.fnamemodify(vim.fn.resolve(file), ':p')
     if vim.endswith(file, '/') then
       return file:sub(0, file:len() - 1)
     else

lua/corpus/util/list.lua
@@ -19,6 +19,16 @@ util.list.concat = function(list, other)
   return result
 end
 
+util.list.filter = function(list, cb)
+  local result = {}
+  for i, v in ipairs(list) do
+    if cb(v, i) then
+      table.insert(result, v)
+    end
+  end
+  return result
+end
+
 -- Maps over `list`, returning a new list.
 util.list.map = function(list, cb)
   local result = {}

commit 84d5d62414b6c8fa9d1c378dec6525e00e7f51e1

fix: make symlink resolution work again
Order matters here:

    resolve('~/thing')

doesn't resolve symlinks because it treats "~" literally. In other words:

    fnamemodify(resolve('~/thing'), ':p')

doesn't work, but:

    resolve(fnamemodify('~/thing', ':p'))

does.

I broke this in c255bc375ea95689c45c5812e4fda94f33d99b86 while fixing
another bug (which required me to remove an `expand()` call).

lua/corpus/init.lua
@@ -595,7 +595,7 @@ corpus = {
     if vim.startswith(file, '<') then
       file = vim.fn.expand(file)
     end
-    file = vim.fn.fnamemodify(vim.fn.resolve(file), ':p')
+    file = vim.fn.resolve(vim.fn.fnamemodify(file, ':p'))
     if vim.endswith(file, '/') then
       return file:sub(0, file:len() - 1)
     else